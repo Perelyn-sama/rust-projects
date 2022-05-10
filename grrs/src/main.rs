a8c8d24404a1d3b71fc22fa5b199ad0ff2e17621
Perelyn-sama <okohebina@gmail.com>
2022-05-10 12:21:36 +0100

    part one

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+#![allow(unused)]
+use clap::Parser;
+
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(Parser)]
+struct Cli {
+    /// The pattern to look for
+    pattern: String,
+    /// The path to the file to read
+    #[clap(parse(from_os_str))]
+    path: std::path::PathBuf
 }
+
+
+fn main() {
+    let args = Cli::parse();
+
+    // let content = std::fs::read_to_string(&args.path)
+    //     .expect("could not read file");
+
+    let resulut = std::fs::read_to_string("test.txt");
+    let content = match resulut {
+        Ok(content) => {content},
+        Err(error) => {panic!("Cant't deal with {}, just exit here", error);}
+    };
+    println!("file content: {}", content);
+
+    for line in content.lines() {
+        if line.contains(&args.pattern){
+            println!("{}", line);
+            
+        }
+    }
+}
+
+
+// let pattern = std::env::args().nth(1).expect("no pattern given");
+// let path = std::env::args().nth(2).expect("no path given");
---
+++ minigrep/src/main.rs
@@ -0,0 +1,54 @@
+use std::{env};
+use std::fs;
+use std::process;
+use std::error::Error;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    // let query: &String = &args[1];
+    // let filename: &String = &args[2];
+
+    let config = Config::new(&args).unwrap_or_else(| err | {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+    
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+
+    let contents: String = fs::read_to_string(config.filename)?;
+
+    print!("With test:\n{}", contents);
+
+    Ok(())
+
+} 
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &str> {
+
+        if args.len() < 3{
+            return  Err("Not enough arguements");
+        }
+
+        let query: String = args[1].clone();
+        let filename: String = args[2].clone();
+    
+        Ok(Config { query, filename })
+    }    
+}
+
